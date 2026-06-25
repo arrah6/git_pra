42e22402e860839ea5d15b1a143dcfddbcfc7d9a
arrah6 <abdular2003@outlook.com>
2026-06-25 10:47:50 +0530

    second commit

--- ar.java
+++ ar.java
@@ -2,5 +2,6 @@ public class ar {
     public static void main(String[] args) {
         System.out.println("hi");
         System.out.println("a new branch created..");
+        System.out.println("branch 2 is created..");
     }
 }
